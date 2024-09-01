aa8187a2ee489f0df5ed6348e133947b979b2788
comstering <coms1101@gmail.com>
2024-09-01 20:00:35 +0900

    refactor: Rename module name

---
+++ event-core/build.gradle.kts
@@ -0,0 +1,18 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = rootProject.group
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
