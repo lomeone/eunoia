13be2fefde4f7d31799eb5c6375da8237e4a06ed
comstering <coms1101@gmail.com>
2024-09-01 20:42:45 +0900

    feat: Add dependency for event in transactional outbox

--- event-spring-transactional-outbox/build.gradle.kts
+++ event-spring-transactional-outbox/build.gradle.kts
@@ -9,11 +9,26 @@ plugins {
 group = "${rootProject.group}.event.spring.transactional.outbox"
 version = "1.0-SNAPSHOT"
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(project(":event-core"))
+
     // DB
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     kapt("jakarta.persistence:jakarta.persistence-api")
