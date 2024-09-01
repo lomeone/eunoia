8193dab5455274ee07fe4ba5ef3740791359a6a1
comstering <coms1101@gmail.com>
2024-09-01 20:35:43 +0900

    feat: Add pulgin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,15 +3,23 @@ rootProject.name = projectName
 
 pluginManagement {
     val kotlinVersion: String by settings
+    val springBootVersion: String by settings
+    val springDependencyManagement: String by settings
     val shadowVersion: String by settings
 
     plugins {
         kotlin("jvm") version kotlinVersion
+        kotlin("plugin.spring") version kotlinVersion
+        kotlin("kapt") version kotlinVersion
+        kotlin("plugin.jpa") version kotlinVersion
+        id("org.springframework.boot") version springBootVersion
+        id("io.spring.dependency-management") version springDependencyManagement
         id("com.gradleup.shadow") version shadowVersion
     }
 }
 
 
-include("exception")
-include("transaction-outbox")
 include("event-core")
+include("event-spring-transactional-outbox")
+include("exception")
+include("transactional-outbox")
