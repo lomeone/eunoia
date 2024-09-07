4df7dfbaf38b57817f32d6d69035c52bdcc5ea07
comstering <coms1101@gmail.com>
2024-09-07 20:30:02 +0900

    refactor: Refactor outbox entity

--- event-spring-transactional-outbox/src/main/kotlin/com/eunoia/event/spring/transactional/outbox/event/kafka/KafkaOutbox.kt
+++ event-spring-transactional-outbox/src/main/kotlin/com/eunoia/event/spring/transactional/outbox/event/kafka/KafkaOutbox.kt
@@ -1,10 +1,12 @@
 package com.eunoia.event.spring.transactional.outbox.event.kafka
 
+import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDateTime
 import java.time.LocalDateTime.now
 
@@ -16,10 +18,15 @@ class KafkaOutbox(
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0L,
     val topic: String,
-    val partitionKey: String,
-    val messageBody: String,
-    val messageHeaders: String
+    val key: String,
+    val payload: String,
+    val metadata: String
 ) {
     @CreatedDate
+    @Column(updatable = false)
     val createdAt: LocalDateTime = now()
+
+    @LastModifiedDate
+    var updatedAt: LocalDateTime = now()
+        protected set
 }
