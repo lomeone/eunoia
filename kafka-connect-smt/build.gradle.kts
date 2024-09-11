209bb5d4bb6f22ecd38fafc24cab2ca8e22d1fef
comstering <coms1101@gmail.com>
2024-09-11 17:50:34 +0900

    refactor: Remove not need library

--- kafka-connect-smt/build.gradle.kts
+++ kafka-connect-smt/build.gradle.kts
@@ -2,8 +2,8 @@ group = "${rootProject.group}.kafka.connect.smt"
 version = "1.0-SNAPSHOT"
 
 val kafkaConnectVersion: String by project
-val debeziumVersion: String by project
 val kotlinSerializationVersion: String by project
+val slf4jApiVersion: String by project
 
 plugins {
     id("com.gradleup.shadow")
@@ -26,11 +26,11 @@ dependencies {
     // Kafka Connect API
     implementation("org.apache.kafka:connect-api:$kafkaConnectVersion")
 
-    // Debezium Core
-    implementation("io.debezium:debezium-core:$debeziumVersion")
-
     // kotlin serialization
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinSerializationVersion")
+
+    // slf4j log
+    implementation("org.slf4j:slf4j-api:$slf4jApiVersion")
 }
 
 tasks.test {
