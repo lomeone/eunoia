48bab444a695e8ab1f0223af3eb112fa0a281f71
comstering <coms1101@gmail.com>
2024-09-12 18:03:35 +0900

    feat: Implement kafka event producer base

---
+++ event-core/src/main/kotlin/com/eunoia/event/kafka/KafkaEventProducer.kt
@@ -0,0 +1,51 @@
+package com.eunoia.event.kafka
+
+import com.eunoia.event.EventProducer
+import io.cloudevents.CloudEvent
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeaders
+import org.apache.kafka.common.serialization.StringDeserializer
+import java.util.*
+
+class KafkaEventProducer(
+    brokerServers: String
+) : EventProducer {
+    val kafkaProducer: KafkaProducer<String, CloudEvent>
+    init {
+        val props = Properties()
+        props["bootstrap.servers"] = brokerServers
+        props["key.deserializer"] = StringDeserializer::class.java.canonicalName
+        props["value.deserializer"] = "io.cloudevents.kafka.CloudEventSerializer"
+        kafkaProducer = KafkaProducer(props)
+    }
+
+    override fun produce(destination: String, event: CloudEvent) {
+        val key = event.id
+        val headers = generateHeader(event)
+
+        val record = ProducerRecord(destination, null, key, event, headers)
+
+        kafkaProducer.send(record) { metadata, exception ->
+            if (exception != null) {
+                println("Error sending message: ${exception.message}")
+            } else {
+                println("Message sent to topic ${metadata.topic()} with offset ${metadata.offset()}")
+            }
+        }
+    }
+
+    private fun generateHeader(event: CloudEvent): RecordHeaders {
+        val headers = RecordHeaders()
+        val headerData = event.attributeNames
+
+        for (key in headerData) {
+            val value = event.getAttribute(key)
+            if (value != null ) {
+                headers.add(key, value.toString().toByteArray())
+            }
+        }
+
+        return headers
+    }
+}
