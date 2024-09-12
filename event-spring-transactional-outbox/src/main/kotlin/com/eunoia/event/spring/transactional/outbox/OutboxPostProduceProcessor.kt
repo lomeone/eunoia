8326b78808f843b64f09c100d22c1acec7d742ce
comstering <coms1101@gmail.com>
2024-09-12 14:23:48 +0900

    refactor: Remove don't need import

--- event-spring-transactional-outbox/src/main/kotlin/com/eunoia/event/spring/transactional/outbox/OutboxPostProduceProcessor.kt
+++ event-spring-transactional-outbox/src/main/kotlin/com/eunoia/event/spring/transactional/outbox/OutboxPostProduceProcessor.kt
@@ -2,11 +2,9 @@ package com.eunoia.event.spring.transactional.outbox
 
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
-import org.springframework.stereotype.Service
 import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
-@Service
 class OutboxPostProduceProcessor(
     private val outboxRemover: OutboxRemover
 ) {
@@ -16,7 +14,7 @@ class OutboxPostProduceProcessor(
     @TransactionalEventListener
     fun processOnOutboxProduce(outbox: Outbox) {
         try {
-        outboxRemover.removeOutboxData(outbox)
+            outboxRemover.removeOutboxData(outbox)
         } catch (e: Exception) {
             logger.error("Failed remove outbox message {}", e.toString())
         }
