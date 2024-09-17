8d750f9d2f58988ed572ba40a8b7d213508c1aeb
comstering <coms1101@gmail.com>
2024-09-17 23:17:10 +0900

    refactor: Refactor error code class

--- exception/src/main/kotlin/com/eunoia/exception/EunioaException.kt
+++ exception/src/main/kotlin/com/eunoia/exception/EunioaException.kt
@@ -1,12 +1,16 @@
 package org.example.com.eunoia.exception
 
 open class EunioaException(
-    val errorCode: String,
     message: String,
-    val exceptionCategory: ExceptionCategory,
+    val errorCode: ErrorCode,
     val detail: Map<String, Any> = mapOf(),
     cause: Throwable? = null
 ) : RuntimeException(message, cause)
+
+data class ErrorCode(
+    val code: String,
+    val exceptionCategory: ExceptionCategory
+)
 
 enum class ExceptionCategory {
     BAD_REQUEST,
