760184c47ff6923d9ec455809e1c768a77a971df
comstering <comstering@gmail.com>
2025-01-30 22:13:40 +0900

    feat: Add spring dgs module

---
+++ spring-web-dgs/build.gradle.kts
@@ -0,0 +1,17 @@
+plugins {
+    kotlin("jvm")
+}
+
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
