5bb49bf27b9c96309e8faa8af74db66db90a4728
comstering <comstering@gmail.com>
2025-04-24 08:56:15 +0900

    feat: Implement password util for cheking password validaity

---
+++ kotlin-util/src/main/kotlin/com/lomeone/eunoia/kotlin/util/security/authentication/PasswordUtils.kt
@@ -0,0 +1,20 @@
+package com.lomeone.eunoia.kotlin.util.security.authentication
+
+import com.lomeone.eunoia.kotlin.util.security.authentication.exception.PasswordInvalidException
+import com.lomeone.eunoia.kotlin.util.security.authentication.exception.PasswordIsBlankException
+
+object PasswordUtils {
+    fun checkPasswordValidity(password: String) {
+        checkPasswordIsNotBlank(password)
+        checkPasswordFormatValid(password)
+    }
+
+    private fun checkPasswordIsNotBlank(password: String) {
+        password.isBlank() && throw PasswordIsBlankException(mapOf("password" to password))
+    }
+
+    private fun checkPasswordFormatValid(password: String) {
+        val regex = Regex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\$@\$!%*?&])[A-Za-z\\d\$@!%*?&]{10,}")
+        !regex.matches(password) && throw PasswordInvalidException(mapOf("password" to password))
+    }
+}
