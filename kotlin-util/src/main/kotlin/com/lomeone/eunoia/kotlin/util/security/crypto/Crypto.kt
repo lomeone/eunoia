c88c0fbe8e53d203a6a1948c4981fe7e2c16da19
comstering <comstering@gmail.com>
2025-12-22 22:55:03 +0900

    refactor: Refactor crypto interface

--- kotlin-util/src/main/kotlin/com/lomeone/eunoia/kotlin/util/security/crypto/Crypto.kt
+++ kotlin-util/src/main/kotlin/com/lomeone/eunoia/kotlin/util/security/crypto/Crypto.kt
@@ -1,10 +1,6 @@
 package com.lomeone.eunoia.kotlin.util.security.crypto
 
-import javax.crypto.spec.SecretKeySpec
-
-fun getKeySpec(key: String, algorithm: String): SecretKeySpec = SecretKeySpec(key.toByteArray(), algorithm)
-
 interface Crypto {
-    fun encrypt(plainText: String, key: String): String
-    fun decrypt(cipherText: String, key: String): String
+    fun encrypt(data: ByteArray): ByteArray
+    fun decrypt(encryptedData: ByteArray): ByteArray
 }
