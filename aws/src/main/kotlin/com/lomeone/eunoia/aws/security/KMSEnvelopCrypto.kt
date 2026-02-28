e6f7cc5d2ea6d75f718eae0f75605c7defe50ed9
comstering <comstering@gmail.com>
2026-02-28 19:02:24 +0900

    refactor: Refactor kms ecnrypt package

--- aws/src/main/kotlin/com/lomeone/eunoia/aws/security/KMSEnvelopCrypto.kt
+++ aws/src/main/kotlin/com/lomeone/eunoia/aws/security/KMSEnvelopCrypto.kt
@@ -6,32 +6,48 @@ import aws.sdk.kotlin.services.kms.generateDataKey
 import aws.sdk.kotlin.services.kms.model.DataKeySpec
 import com.lomeone.eunoia.security.crypto.Crypto
 import com.lomeone.eunoia.security.crypto.symmetric.AESGCMCrypto
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class KMSEnvelopeCrypto(
     private val kmsClient: KmsClient,
     private val keyArn: String
 ) : Crypto {
-    val dataKey: ByteArray
-    val encryptedDataKey: ByteArray
+    data class KMSKeys(
+        val plainKey: ByteArray,
+        val encryptedKey: ByteArray
+    )
 
-    init {
-        val generateDataKeyResponse = runBlocking {
-            kmsClient.generateDataKey {
-                keyId = keyArn
-                keySpec = DataKeySpec.Aes256
+    private var cachedKeys: KMSKeys? = null
+    private val mutex = Mutex()
+
+    private suspend fun getKeys(): KMSKeys {
+        cachedKeys?.let { return it }
+
+        return mutex.withLock {
+            cachedKeys ?: run {
+                val response = kmsClient.generateDataKey {
+                    keyId = keyArn
+                    keySpec = DataKeySpec.Aes256
+                }
+
+                KMSKeys(
+                    plainKey = response.plaintext ?: throw IllegalStateException("Unable to generate dataKey"),
+                    encryptedKey = response.ciphertextBlob ?: throw IllegalStateException("Unable to generate encryptedDataKey")
+                ).also { cachedKeys = it }
             }
         }
-
-        dataKey = generateDataKeyResponse.plaintext ?: throw IllegalStateException("Unable to generate dataKey")
-        encryptedDataKey = generateDataKeyResponse.ciphertextBlob ?: throw IllegalStateException("Unable to generate encryptedDataKey")
     }
 
-    override fun encrypt(data: ByteArray): ByteArray {
-        return packageInEnvelope(AESGCMCrypto(dataKey).encrypt(data))
+    override suspend fun encrypt(data: ByteArray): ByteArray {
+        val (dataKey, encryptedDataKey) = getKeys()
+        return packageInEnvelope(
+            encryptedDataKey = encryptedDataKey,
+            encryptedData = AESGCMCrypto(dataKey).encrypt(data)
+        )
     }
 
-    private fun packageInEnvelope(encryptedData: ByteArray): ByteArray {
+    private fun packageInEnvelope(encryptedDataKey: ByteArray, encryptedData: ByteArray): ByteArray {
         val envelope = ByteArray(2 + encryptedDataKey.size + encryptedData.size)
 
         // https://docs.aws.amazon.com/encryption-sdk/latest/developer-guide/message-format.html#data-key-length
@@ -44,14 +60,12 @@ class KMSEnvelopeCrypto(
         return envelope
     }
 
-    override fun decrypt(encryptedData: ByteArray): ByteArray {
+    override suspend fun decrypt(encryptedData: ByteArray): ByteArray {
         val (encryptedDataKey, ciphertextBytes) = unpackageFromEnvelope(encryptedData)
 
-        val dataKey = runBlocking {
-            kmsClient.decrypt {
-                ciphertextBlob = encryptedDataKey
-                keyId = keyArn
-            }
+        val dataKey = kmsClient.decrypt {
+            ciphertextBlob = encryptedDataKey
+            keyId = keyArn
         }.plaintext ?: throw IllegalStateException("Unable to decrypt dataKey")
 
         return AESGCMCrypto(dataKey).decrypt(ciphertextBytes)
